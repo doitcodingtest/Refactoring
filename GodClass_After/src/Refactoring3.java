eecfcf9b5c982e1d5b4791bc5bf2c1ec038a36e6
todayCoding <codingtest@naver.com>
2025-03-27 22:40:50 +0900

    리팩터링 코드 수정

--- GodClass_After/src/Refactoring3.java
+++ GodClass_After/src/Refactoring3.java
@@ -1,8 +1,9 @@
 public class Refactoring3 {
     public static void main(String[] args) {
-        Cellphone cellphone = new Cellphone("갤럭시","파란색",175,"173mm","3088 x 1440 (Quad HD+)"
-                ,"Dynamic AMOLED 2X","120Hz", "16M",5000,25,
-                25,26,false);
+        DisplaySpec displaySpec = new DisplaySpec("173mm","3088 x 1440 (Quad HD+)","Dynamic AMOLED 2X","120Hz", "16M");
+        BatterySpec batterySpec = new BatterySpec(5000,25, 25,26,false);
+        CellphoneSpec cellphoneSpec = new CellphoneSpec(displaySpec, batterySpec);
+        Cellphone cellphone = new Cellphone("갤럭시","파란색",175,cellphoneSpec);
         System.out.println(cellphone.toString());
     }
 }
@@ -11,28 +12,79 @@ class Cellphone{
     String name;
     String color;
     int price;
-    // 디스플레이 관련 변수
+    CellphoneSpec cellphoneSpec;
+
+    public Cellphone(String name, String color, int price, CellphoneSpec cellphoneSpec) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+        this.cellphoneSpec = cellphoneSpec;
+    }
+
+    @Override
+    public String toString() {
+        return "Cellphone{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                ", cellphoneSpec=" + cellphoneSpec.toString() +
+                '}';
+    }
+}
+
+class CellphoneSpec{
+    DisplaySpec displaySpec;
+    BatterySpec batterySpec;
+
+    public CellphoneSpec(DisplaySpec displaySpec, BatterySpec batterySpec) {
+        this.displaySpec = displaySpec;
+        this.batterySpec = batterySpec;
+    }
+
+    @Override
+    public String toString() {
+        return "CellphoneSpec{" +
+                "displaySpec=" + displaySpec.toString() +
+                ", batterySpec=" + batterySpec.toString() +
+                '}';
+    }
+}
+
+class DisplaySpec{
     String displaySize;
     String  resolution;
     String displayType;
     String maxRefreshRate;
     String colorDepth;
-    // 배터리 관련 변수
+
+    public DisplaySpec(String displaySize, String resolution, String displayType, String maxRefreshRate, String colorDepth) {
+        this.displaySize = displaySize;
+        this.resolution = resolution;
+        this.displayType = displayType;
+        this.maxRefreshRate = maxRefreshRate;
+        this.colorDepth = colorDepth;
+    }
+
+    @Override
+    public String toString() {
+        return "DisplaySpec{" +
+                "displaySize='" + displaySize + '\'' +
+                ", resolution='" + resolution + '\'' +
+                ", displayType='" + displayType + '\'' +
+                ", maxRefreshRate='" + maxRefreshRate + '\'' +
+                ", colorDepth='" + colorDepth + '\'' +
+                '}';
+    }
+}
+
+class BatterySpec{
     int batteryCapacity;
     int internetUsageTime;
     int videoPlaybackTime;
     int audioPlaybackTime;
     boolean isReplaceable;
 
-    public Cellphone(String name, String color, int price, String displaySize, String resolution, String displayType, String maxRefreshRate, String colorDepth, int batteryCapacity, int internetUsageTime, int videoPlaybackTime, int audioPlaybackTime, boolean isReplaceable) {
-        this.name = name;
-        this.color = color;
-        this.price = price;
-        this.displaySize = displaySize;
-        this.resolution = resolution;
-        this.displayType = displayType;
-        this.maxRefreshRate = maxRefreshRate;
-        this.colorDepth = colorDepth;
+    public BatterySpec(int batteryCapacity, int internetUsageTime, int videoPlaybackTime, int audioPlaybackTime, boolean isReplaceable) {
         this.batteryCapacity = batteryCapacity;
         this.internetUsageTime = internetUsageTime;
         this.videoPlaybackTime = videoPlaybackTime;
@@ -42,16 +94,8 @@ class Cellphone{
 
     @Override
     public String toString() {
-        return "Cellphone{" +
-                "name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                ", price=" + price +
-                ", displaySize='" + displaySize + '\'' +
-                ", resolution='" + resolution + '\'' +
-                ", displayType='" + displayType + '\'' +
-                ", maxRefreshRate='" + maxRefreshRate + '\'' +
-                ", colorDepth='" + colorDepth + '\'' +
-                ", batteryCapacity=" + batteryCapacity +
+        return "BatterySpec{" +
+                "batteryCapacity=" + batteryCapacity +
                 ", internetUsageTime=" + internetUsageTime +
                 ", videoPlaybackTime=" + videoPlaybackTime +
                 ", audioPlaybackTime=" + audioPlaybackTime +
